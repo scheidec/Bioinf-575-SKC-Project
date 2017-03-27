61942f8f4abf84dded2669f85c4903569447019e
samuelephant <samuelephant@gmail.com>
2017-03-27 10:20:08 -0400

    Add files via upload

---
+++ feature_extraction.py
@@ -0,0 +1,36 @@
+#!/urs/env/bin python
+""" Feature Extraction
+"""
+
+__author__ = 'Nanxiang Zhao'
+__email__ = 'samzhao@umich.edu'
+
+
+#rna = 'GATCATCGGATGCTTAGGGGGATCGATCGGAGGCATT'
+
+def monofreq(seq):
+
+    all_counts = {}
+    for base in ['A', 'T', 'G', 'C']:
+        count = seq.count(base)
+        all_counts[base] = count
+    for key in all_counts:
+        all_counts[key] = float(all_counts[key]) / len(seq)
+    return(all_counts)
+
+#print monofreq(rna)
+
+def difreq(seq):
+
+    all_counts = {}
+    for base1 in ['A', 'T', 'G', 'C']:
+        for base2 in ['A', 'T', 'G', 'C']:
+            dinucleotide = base1 + base2
+            count = seq.count(dinucleotide)
+            all_counts[dinucleotide] = count
+    for key in all_counts:
+        length = len(seq) - 1
+        all_counts[key] = float(all_counts[key]) / length
+    return(all_counts)
+
+#print difreq(rna)
